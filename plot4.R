58a6e59678af4ce6c8d97cd4a0d5e4ab069c1c62
Manuel <Manuelgarcia123@hotmail.com>
2015-08-20 22:25:54 -0500

    plots corrected

--- plot4.R
+++ plot4.R
@@ -13,18 +13,20 @@ plot4 <- function(){
   x <- paste(dates,times)
   useful$Date <- strptime(x,"%d/%m/%Y %H:%M:%S")
   par(mfcol=c(2,2))
+  Sys.setlocale("LC_ALL","English")
   plot(useful$Date,useful$Global_active_power,type = "l",xlab = "",
        ylab = "Global Active Power")
   plot(useful$Date,useful$Sub_metering_1,type = "n",xlab = "",ylab = "Energy sub metering")
   lines(useful$Date,useful$Sub_metering_1,col="black")
   lines(useful$Date,useful$Sub_metering_2,col="red")
   lines(useful$Date,useful$Sub_metering_3,col="blue")
-  legend(x=3,y=30,c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-         col=c("black","red","blue"),lty = 1,cex = 0.3,bty="n")
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),cex=0.8)
   plot(useful$Date,useful$Voltage,type="l",xlab = "",ylab = "Voltage")
   plot(useful$Date,useful$Global_reactive_power,xlab = "datetime",type="l",
        ylab = "Global_reactive_power")
   dev.copy(png,file = "plot4.png")
   dev.off()
   "Plot 4 created"
+  Sys.setlocale("LC_ALL","Spanish")
 }
